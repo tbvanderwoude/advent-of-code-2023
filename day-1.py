fb1536e5060e5751ba1e646b2aa63d626cffa8de
Thom van der Woude <tbvanderwoude@gmail.com>
2023-12-03 17:17:02 +0100

    Initial commit

---
+++ day-1.py
@@ -0,0 +1,40 @@
+def solver(text, allow_text_digits = False):
+    digit_map = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+    callibration_values = []
+    for line in text.splitlines():
+        numbers = [] 
+        i = 0
+        for c in line:
+            if c.isdecimal():
+                numbers.append(c)
+            if allow_text_digits:
+                subline = line[i:]
+                for digit in digit_map:
+                    index = subline.find(digit) 
+                    if index != -1 and index == 0:
+                        numbers.append(digit_map[digit])
+                        continue
+            i += 1
+        # Strings of first and last number
+        first = numbers[0]
+        last = numbers[-1]
+        cal_str = first + last
+        callibration_values.append(int(cal_str))
+    cal_sum = sum(callibration_values)
+    print(cal_sum)
+
+if __name__ == '__main__':
+    with open("inputs/day-1.txt") as f:
+        text = f.read()
+        solver(text)
+        solver(text,True)
