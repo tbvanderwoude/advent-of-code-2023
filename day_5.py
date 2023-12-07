f7024cfcc4177096dd4c85b390e6b787e345f0c7
Thom van der Woude <tbvanderwoude@gmail.com>
2023-12-07 11:56:14 +0100

    Created Interval class and refactored day 5 part 1 solution to use it

--- day_5.py
+++ day_5.py
@@ -2,23 +2,49 @@ from aoc_util import *
 from pprint import pprint
 from itertools import permutations
 
-class Range:
-    def __init__(self, source, dest, length):
-        self.source = source
-        self.dest = dest
-        self.length = length
+class Interval:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
     
     def contains(self, x):
-        return self.source <= x < self.source + self.length
+        return self.a <= x and x <= self.b
     
+    def overlap(self, other):
+        return self.b >= other.a and self.a <= other.b
+    
+    def __add__(self, value):
+        return Interval(self.a + value,self.b + value)
+    
+    def intersect(self, other):
+        if self.overlap(other):
+            return Interval(max(self.a,other.a),min(self.b,other.b))
+        else:
+            raise ValueError("There is no intersection")
+        
     def __repr__(self):
-        return f"Range({self.source} - {self.source + self.length - 1} => {self.dest} - {self.dest + self.length - 1})"
-    def map(self,x):
+        return f"[{self.a} - {self.b}]"
+        
+class Range:
+    def __init__(self, source_interval, shift):
+        self.source_interval = source_interval
+        self.shift = shift
+    
+    def contains(self, x):
+        return self.source_interval.contains(x)
+    
+    def map_val(self,x):
         if self.contains(x):
-            return self.dest + (x - self.source)
+            return x + self.shift
         else:
             raise ValueError("Cannot map {x}")
 
+    def map_interval(self,x):
+        
+        if self.contains(x):
+            return x + self.shift
+        else:
+            raise ValueError("Cannot map {x}")
 
 class RangeMap:
     def __init__(self, from_name, to_name, ranges):
@@ -29,7 +55,7 @@ class RangeMap:
     def get(self, i):
         for range in self.ranges:
             if range.contains(i):
-                return range.map(i)
+                return range.map_val(i)
         return i
 
 lines = load_input(5)
@@ -44,8 +70,10 @@ for map_str in map_strs:
     from_name, to_name = lines[0].removesuffix(' map:').split('-to-')
     ranges = []
     for map in lines[1:]:
-        numbers = read_numbers(map)
-        ranges.append(Range(numbers[1],numbers[0],numbers[2]))
+        dest_start, source_start, width = read_numbers(map)
+        interval = Interval(source_start,source_start + width - 1)
+        shift = dest_start - source_start
+        ranges.append(Range(interval,shift))
     range_map = RangeMap(from_name, to_name, ranges)
     map_by_name[from_name] = range_map
 
@@ -62,4 +90,8 @@ for seed in seeds:
     locations.append(val)
 part_1 = min(locations)
 print(part_1)
+
+i_a = Interval(2,6)
+i_b = Interval(5,10)
+print(i_a.intersect(i_b))
 # print(maps_str)
