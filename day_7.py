aaac8a102220112e4ff4df2a5a378262dcead3db
Thom van der Woude <tbvanderwoude@protonmail.com>
2023-12-08 15:16:44 +0100

    Added comment about current mistake in part 2 solution

--- day_7.py
+++ day_7.py
@@ -12,10 +12,31 @@ class HandType(IntEnum):
     OnePair = 1
     HighCard = 0
 
-def determine_type(s):
+# Logic depends on which part is being solved
+# Lord have mercy upon me
+PART = 1
+
+cards_1 = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3','2']
+
+cards_2 = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3','2', 'J']
+
+def determine_type(s, jokers = False):
     counter = Counter(s)
-    counts = sorted(list(counter.items()), key=lambda x: -x[1])
+    counter_map = dict(counter)
+    # Current problem: there is no tie-breaking depending on card value where the joker assumes the value of the highest card of a certain value
+    # For instance, "AAKKJ" should become a Three of A not K
+    counts = list(sorted(list(counter.items()), key=lambda x: -x[1]))
     highest = counts[0][1]
+    if jokers:
+        highest_is_joker = counts[0][0] == 'J'
+        if not highest_is_joker and 'J' in counter_map:
+            no_jokers = counter_map['J']
+            print(f"{s} contains {no_jokers} jokers; Increasing highest ({highest}) by {no_jokers}")
+            highest += no_jokers
+            for i in range(len(counts)):
+                if counts[i][0] == 'J':
+                    counts[i] = ('J',0)
+                    break    
     match highest:
         case 5:
             return HandType.Five
@@ -36,17 +57,18 @@ def determine_type(s):
         case 1:
             return HandType.HighCard
 
-cards = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3','2']
 @functools.total_ordering
 class Hand:
-    def __init__(self,s,bid):
+    def __init__(self,s,bid = 0):
         self.s = s
         self.bid = bid
-        self.type = determine_type(s)
+        self.type = determine_type(s, jokers = PART == 2)
+
     def __eq__(self, other):
         return self.s == other.s
     
     def get_card_scores(self):
+        cards = cards_1 if PART == 1 else cards_2
         return [len(cards) - cards.index(c) for c in self.s]
     def __lt__(self, other):
         if self.type != other.type:
@@ -70,5 +92,20 @@ for line in lines:
     hands.append(hand)
 
 sorted_cards = list(enumerate(sorted(hands),1))
+print(sorted_cards)
+
 total_winnings = sum([h.bid * i for (i,h) in sorted_cards])
 print(total_winnings)
+
+PART = 2
+
+for hand in hands:
+    hand.type = determine_type(hand.s, jokers = PART == 2)
+    print(hand,hand.type,'\n')
+
+sorted_cards = list(enumerate(sorted(hands),1))
+print(sorted_cards)
+total_winnings = sum([h.bid * i for (i,h) in sorted_cards])
+print(total_winnings)
+
+assert(Hand("QQQQ2") > Hand("JKKK2"))
